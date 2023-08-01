181fa4b79be965e15cceadd94eec299eb33f7abb
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-08-01 12:40:23 -0300

    Add files via upload

---
+++ Funcoes/funcoes.js
@@ -0,0 +1,11 @@
+saldo = 1000000000
+function dadosacesso(){
+    numecartao = prompt ("Digite o número do cartão!")
+    senha = prompt("Digite a Senha: ")
+}
+
+function saque(valor){
+    saldo = saldo - valor
+    return saldo
+}
+
