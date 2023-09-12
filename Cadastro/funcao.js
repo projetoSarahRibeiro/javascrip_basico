457f7cb2cb96e3c10c6d62e8ebbfafb5c22917cc
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-09-12 10:26:13 -0300

    Add files via upload

---
+++ Cadastro/funcao.js
@@ -0,0 +1,18 @@
+var nome
+var email
+var num = 0; 
+saldo = 1000;
+var saque = 0;
+
+function comretorno(saque){
+    saldo = saldo- saque
+    return saldo 
+}
+
+function escrever(quadro){
+    info = "Usuário número " + (++num) + "\n";
+    info += "Nome:" + nome + "\n";
+    info +="Email:" + email + "\n";
+    info += "Saldo:" + comretorno(saque) + "\n \n";
+    quadro.value += info;
+}
